5d8a89a2b17ebda6bbee9068acf4b025d7d2f028
culveyhouse <dculvey@gmail.com>
2016-01-20 16:12:51 +0000

    last commit before branching for the first time

--- homesnacksweb/models.py
+++ homesnacksweb/models.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import User
 
 # Create your models here.
