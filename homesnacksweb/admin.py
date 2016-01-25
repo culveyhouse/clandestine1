3fff64855daeda3e10ed2466e710902981111104
culveyhouse <dculvey@gmail.com>
2016-01-25 15:50:44 +0000

    Most models added now, including DataCycle and DataCycleSteps

--- homesnacksweb/admin.py
+++ homesnacksweb/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import MLS, PropertyCurrent
+from .models import MLS, PropertyCurrent, DataCycle, DataCycleStep
 
 # Register your models here.
 admin.site.register(MLS)
 admin.site.register(PropertyCurrent)
+admin.site.register(DataCycle)
+admin.site.register(DataCycleStep)
 
--- homesnacksweb/models.py
+++ homesnacksweb/models.py
@@ -1,8 +1,8 @@
 from __future__ import unicode_literals
-
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
 
@@ -23,7 +23,6 @@ class MLS(models.Model):
 class PropertyCurrent(models.Model):
     """PropertyCurrent is the model/table that holds all normalized property data 
     that was most recently used to generate the full set of static HTML listing pages"""
-    
     STATUS_ACTIVE = 1
     STATUS_PENDING = 2
     STATUS_SOLD = 3
@@ -62,21 +61,84 @@ class PropertyCurrent(models.Model):
     bedrooms_half = models.DecimalField(decimal_places=2, max_digits=5, default=0, null=True, blank=True)
     bathrooms_half = models.DecimalField(decimal_places=2, max_digits=5, default=0, null=True, blank=True)
     rooms_total = models.DecimalField(decimal_places=2, max_digits=5, default=0, null=True, blank=True)        
-    list_date = models.DateTimeField(default='2015-01-01', null=True, blank=True)        
+    list_date = models.DateField(default=date(2015, 1, 1) , null=True, blank=True)        
     size = models.PositiveIntegerField(default=0)
     geo_lat = models.DecimalField(decimal_places=12, max_digits=24, null=True, blank=True)
     geo_long = models.DecimalField(decimal_places=12, max_digits=24, null=True, blank=True)
     photo_count = models.PositiveIntegerField(default=0)
     status = models.PositiveSmallIntegerField(choices=PROPERTY_STATUS, default=STATUS_ACTIVE,
-        help_text='Choose whether this is a main section, a subsection, or a third-level section',
+        help_text='For sale / sold status of the property',
         validators=[MinValueValidator(1),
-                    MaxValueValidator(3)])
+                    MaxValueValidator(4)])
     date_added = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     
     class Meta:
         verbose_name_plural = "PropertiesCurrent"
         
-        
-        
-        
+class DataCycle(models.Model):
+    """The DataCycle model maintains rows of data cycle runs, 
+    and records current status of all parts of the data cycle"""
+    STATUS_NOT_STARTED = 1
+    STATUS_RUNNING = 2
+    STATUS_COMPLETE = 3
+    STATUS_COMPLETE_W_ERROR = 4
+    STATUS_ABEND = 5 
+    JOB_STATUS = (
+        (STATUS_NOT_STARTED, 'Not Started'),
+        (STATUS_RUNNING, 'Running'),
+        (STATUS_COMPLETE, 'Finished'),
+        (STATUS_COMPLETE_W_ERROR, 'Finished with errors'),
+        (STATUS_ABEND, 'Abended/Aborted')
+    )
+    """step1 includes any preparations needed before the MLS feed step begins"""
+    step1_prep_status = models.PositiveSmallIntegerField(choices=JOB_STATUS, default=STATUS_NOT_STARTED)
+    """step2 is the main feed downloader which loops through each MLS data pipe"""
+    step2_download_status = models.PositiveSmallIntegerField(choices=JOB_STATUS, default=STATUS_NOT_STARTED)
+    step3_convert_status = models.PositiveSmallIntegerField(choices=JOB_STATUS, default=STATUS_NOT_STARTED)
+    step4_pagegen_status = models.PositiveSmallIntegerField(choices=JOB_STATUS, default=STATUS_NOT_STARTED)
+    step5_cleanup_status = models.PositiveSmallIntegerField(choices=JOB_STATUS, default=STATUS_NOT_STARTED)
+    last_step_completed = models.PositiveSmallIntegerField(default=0)    
+    data_cycle_start = models.DateTimeField(null=True, blank=True)
+    data_cycle_finish = models.DateTimeField(null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+               
+class DataCycleStep(models.Model):
+    """The data cycle steps model is a many-to-one table detailing each step
+    completed of each data cycle, in individual step-rows"""
+    STEP1_PREPARE = 1
+    STEP2_DOWNLOAD = 2
+    STEP3_CONVERT = 3
+    STEP4_GENERATE = 4
+    STEP5_CLEANUP = 5
+    CYCLE_STEPS = (
+        (STEP1_PREPARE, 'Prepare data cycle with pre-download steps'),
+        (STEP2_DOWNLOAD, 'Download/extract all real estate data & media'),
+        (STEP3_CONVERT, 'Normalize all downloaded data into tables'),
+        (STEP4_GENERATE, 'Generate all front-end pages and assets'),
+        (STEP5_CLEANUP, 'Finish post-processing cleanup tasks')
+    )
+    
+    STATUS_NOT_STARTED = 1
+    STATUS_RUNNING = 2
+    STATUS_COMPLETE = 3
+    STATUS_COMPLETE_W_ERROR = 4
+    STATUS_ABEND = 5 
+    JOB_STATUS = (
+        (STATUS_NOT_STARTED, 'Not Started'),
+        (STATUS_RUNNING, 'Running'),
+        (STATUS_COMPLETE, 'Finished'),
+        (STATUS_COMPLETE_W_ERROR, 'Finished with errors'),
+        (STATUS_ABEND, 'Abended/Aborted')
+    )    
+    data_cycle = models.ForeignKey(DataCycle)
+    step_id = models.PositiveSmallIntegerField(choices=CYCLE_STEPS, 
+        validators=[MinValueValidator(1),
+                    MaxValueValidator(5)])
+    step_status = models.PositiveSmallIntegerField(choices=JOB_STATUS, default=STATUS_NOT_STARTED)
+    step_start = models.DateTimeField(null=True, blank=True)
+    step_finish = models.DateTimeField(null=True, blank=True)    
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)    
+    
