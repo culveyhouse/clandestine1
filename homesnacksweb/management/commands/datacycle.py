0bd880d986bbf58a4eb198693a5e51ded5028b6e
culveyhouse <dculvey@gmail.com>
2016-01-29 22:07:16 +0000

    Beginning the data cycle backend system

--- homesnacksweb/management/commands/datacycle.py
+++ homesnacksweb/management/commands/datacycle.py
@@ -1,6 +1,22 @@
 from django.core.management.base import BaseCommand, CommandError
 from homesnacksweb.models import MLS, PropertyCurrent, City, DataCycle, DataCycleStep, JobStatus
-
+from datetime import datetime
 
 class Command(BaseCommand):
     help = 'This command manages the entire HomeSnacks real estate data cycle'
+    
+    def add_arguments(self, parser):
+    # Named (optional) arguments
+        parser.add_argument('--full',
+            action='store_true',
+            dest='full',
+            default=False,
+            help='Run the full data cycle normally, with all steps')
+            
+    def handle(self, *args, **options):
+        if options['full']:
+            self.stdout.write(self.style.SUCCESS('Starting the HomeSnacks data cycle'))   
+            """Add new data cycle to database"""
+            dc = DataCycle(data_cycle_start=datetime.now())
+            dc.save()
+            
