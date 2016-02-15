126d686384ff42844f1d03892e0617643f15b580
culveyhouse <dculvey@gmail.com>
2016-02-15 22:29:58 +0000

    finalized working structure of data cycle command (Django-style)

--- homesnacksweb/management/commands/datacycle.py
+++ homesnacksweb/management/commands/datacycle.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 class Command(BaseCommand):
     help = 'This command manages the entire HomeSnacks real estate data cycle'
+    dc = None
     
     def add_arguments(self, parser):
         """ All optional arguments (--) """    
@@ -23,60 +24,179 @@ class Command(BaseCommand):
             
     def handle(self, *args, **options):
 
-        if options['full'] or (DataCycleStep.STEP1_PREPARE in options['steps']):            
-            self.stdout.write(self.style.SUCCESS('Starting the HomeSnacks data cycle'))   
-            """ Add new data cycle to _datacycle table """
-            dc = DataCycle.create()
-
-
-            """ Step 1 : Prepare """
-            self.stdout.write(self.style.SUCCESS('Executing Step 1: Prepare'))
-            """ Add a data cycle step to _datacyclestep table via a classmethod 
-            and reference data_cycle_id foreign key in dc object """
-            dc_step = DataCycleStep.create_step(DataCycleStep.STEP1_PREPARE, dc)
-            self.stdout.write(self.style.SUCCESS('Data cycle step %s created for dc %s' % (dc_step.step_id, dc.id)))
-            
-            """ pseudo: Finish up any other preparations, then move on to step 2 """
+        """ Step 1 : Prepare """
         
+        if options['full'] or (DataCycleStep.STEP1_PREPARE in options['steps']):            
+            """ Run first step if full data cycle requested or Step 1 explicitly """
+            self.stdout.write(self.style.SUCCESS('Starting the HomeSnacks data cycle'))   
+
+            self.stdout.write(self.style.SUCCESS('Executing Step 1: Prepare'))
+            step1 = Step1Prepare(self)
+            self.dc = step1.execute()
+
         elif options['steps']:
+            """ Otherwise at least grab the last data cycle created to continue with that """
             self.stdout.write(self.style.SUCCESS('Grabbing the most recent data cycle'))   
-            dc = DataCycle.objects.latest('id')
-            
+            self.dc = DataCycle.objects.latest('id')
+            self.stdout.write(self.style.SUCCESS('Skipping Step 1, operating on existing data cycle %s instead' % self.dc.id))
+        """ DLCZZZ: Finish up any other preparations, then move on to step 2 """
+        
+        
+        """ Step 2 : Download """
+        
         if options['full'] or (DataCycleStep.STEP2_DOWNLOAD in options['steps']):      
             
-            """ Step 2 : Download """
             self.stdout.write(self.style.SUCCESS('Executing Step 2: Download'))
-            dc_step = DataCycleStep.create_step(DataCycleStep.STEP2_DOWNLOAD, dc)
-            self.stdout.write(self.style.SUCCESS('Data cycle step %s created for dc %s' % (dc_step.step_id, dc.id)))
-            """ Load list of all MLSs objects that will be downloaded/extracted """
-  
+            step2 = Step2Download(self)
+            dc_step, mls_list = step2.execute()
+            for mls in mls_list:
+                self.stdout.write('mls id is %s' % (mls.id))
+            
+        """ Step 3 : Convert """
+        
         if options['full'] or (DataCycleStep.STEP3_CONVERT in options['steps']):                  
             
-            """ Step 3 : Convert """
             self.stdout.write(self.style.SUCCESS('Executing Step 3: Convert'))
-            dc_step = DataCycleStep.create_step(DataCycleStep.STEP3_CONVERT, dc)
-            self.stdout.write(self.style.SUCCESS('Data cycle step %s created for dc %s' % (dc_step.step_id, dc.id)))            
+            step3 = Step3Convert(self)
+            dc_step = step3.execute()          
 
+        
+        """ Step 4 : Generate """
+        
         if options['full'] or (DataCycleStep.STEP4_GENERATE in options['steps']):      
+            
+            self.stdout.write(self.style.SUCCESS('Executing Step 4: Generate HTML'))
+            step4 = Step4Generate(self)
+            dc_step = step4.execute()          
 
-            """ Step 4 : Generate """
-            self.stdout.write(self.style.SUCCESS('Executing Step 4: Generate'))
-            dc_step = DataCycleStep.create_step(DataCycleStep.STEP4_GENERATE, dc)
-            self.stdout.write(self.style.SUCCESS('Data cycle step %s created for dc %s' % (dc_step.step_id, dc.id)))            
-
+        
+        """ Step 5 : Cleanup """
+        
         if options['full'] or (DataCycleStep.STEP5_CLEANUP in options['steps']):                  
             
-            """ Step 5 : Cleanup """
-            self.stdout.write(self.style.SUCCESS('Executing Step 5: Cleanup'))
-            dc_step = DataCycleStep.create_step(DataCycleStep.STEP5_CLEANUP, dc)
-            self.stdout.write(self.style.SUCCESS('Data cycle step %s created for dc %s' % (dc_step.step_id, dc.id)))
-            
+            self.stdout.write(self.style.SUCCESS('Executing Step 5: Clean Up'))
+            step5 = Step5Cleanup(self)
+            dc_step = step5.execute()       
+    
     
 class Step1Prepare(object):
 
-    def __init__(self):
-        self.step = DataCycleStep.STEP1_PREPARE
+    def __init__(self, dc_cmd):
+        """ Just assign an integer to this step, probably 1 """
+        self.step = DataCycleStep.STEP1_PREPARE 
+        """ Use the passed Command(BaseCommand) object """ 
+        self.dc_cmd = dc_cmd
+        """ Use the generic Command class to access methods like stdout """
+        self.cmd = Command() 
     
-    def step1(self):
-        print(self.step)
+    def execute(self):
+        """ Add new data cycle to _datacycle table """
+        self.cmd.stdout.write(self.cmd.style.SUCCESS('Adding new data cycle...'))
+        self.dc = DataCycle.create()
+        self.cmd.stdout.write(self.cmd.style.SUCCESS('Data cycle %s created' % self.dc.id))
+        
+        """ Add a data cycle step to _datacyclestep table via a classmethod 
+        and reference data_cycle_id foreign key in dc object """
+        dc_step = DataCycleStep.create_step(self.step, self.dc)
+        self.cmd.stdout.write(self.cmd.style.SUCCESS('Data cycle step %s created for data cycle %s (step_id=%s)' 
+            % (dc_step.step_id, self.dc.id, dc_step.id)))
+        
+        """ In this first step class, the entire data cycle object is returned,
+        while in steps 2 - 5, just the current data cycle step object is returned """
+        return self.dc
+
+
+class Step2Download(object):
+
+    def __init__(self, dc_cmd):
+        """ Just assign an integer to this step, probably 1 """
+        self.step = DataCycleStep.STEP2_DOWNLOAD 
+        """ Use the passed Command(BaseCommand) object """ 
+        self.dc_cmd = dc_cmd
+        """ Use the generic Command class to access methods like stdout """
+        self.cmd = Command() 
     
+    def execute(self):
+        """ Add a data cycle step to _datacyclestep table via a classmethod 
+        and reference data_cycle_id foreign key in dc object """
+        dc_step = DataCycleStep.create_step(self.step, self.dc_cmd.dc)
+        self.cmd.stdout.write(self.cmd.style.SUCCESS('Data cycle step %s created for data cycle %s (step_id=%s)' 
+            % (dc_step.step_id, self.dc_cmd.dc.id, dc_step.id)))
+        
+        """ Load list of all MLSs objects that will be downloaded/extracted """
+        mls_list = MLS.objects.all()            
+        for mls in mls_list: 
+            self.cmd.stdout.write('Mls %s' % mls.id)
+            
+        return (dc_step, mls_list)
+
+
+class Step3Convert(object):
+
+    def __init__(self, dc_cmd):
+        """ Just assign an integer to this step, probably 1 """
+        self.step = DataCycleStep.STEP3_CONVERT 
+        """ Use the passed Command(BaseCommand) object """ 
+        self.dc_cmd = dc_cmd
+        """ Use the generic Command class to access methods like stdout """
+        self.cmd = Command() 
+    
+    def execute(self):
+        """ Add a data cycle step to _datacyclestep table via a classmethod 
+        and reference data_cycle_id foreign key in dc object """
+        dc_step = DataCycleStep.create_step(self.step, self.dc_cmd.dc)
+        self.cmd.stdout.write(self.cmd.style.SUCCESS('Data cycle step %s created for data cycle %s (step_id=%s)' 
+            % (dc_step.step_id, self.dc_cmd.dc.id, dc_step.id)))
+        
+        """ Load list of all MLSs objects that will be downloaded/extracted """
+            
+        return dc_step
+
+
+class Step4Generate(object):
+
+    def __init__(self, dc_cmd):
+        """ Just assign an integer to this step, probably 1 """
+        self.step = DataCycleStep.STEP4_GENERATE 
+        """ Use the passed Command(BaseCommand) object """ 
+        self.dc_cmd = dc_cmd
+        """ Use the generic Command class to access methods like stdout """
+        self.cmd = Command() 
+    
+    def execute(self):
+        """ Add a data cycle step to _datacyclestep table via a classmethod 
+        and reference data_cycle_id foreign key in dc object """
+        dc_step = DataCycleStep.create_step(self.step, self.dc_cmd.dc)
+        self.cmd.stdout.write(self.cmd.style.SUCCESS('Data cycle step %s created for data cycle %s (step_id=%s)' 
+            % (dc_step.step_id, self.dc_cmd.dc.id, dc_step.id)))
+        
+        """ Load list of all MLSs objects that will be downloaded/extracted """
+            
+        return dc_step
+
+
+class Step5Cleanup(object):
+
+    def __init__(self, dc_cmd):
+        """ Just assign an integer to this step, probably 1 """
+        self.step = DataCycleStep.STEP5_CLEANUP 
+        """ Use the passed Command(BaseCommand) object """ 
+        self.dc_cmd = dc_cmd
+        """ Use the generic Command class to access methods like stdout """
+        self.cmd = Command() 
+    
+    def execute(self):
+        """ Add a data cycle step to _datacyclestep table via a classmethod 
+        and reference data_cycle_id foreign key in dc object """
+        dc_step = DataCycleStep.create_step(self.step, self.dc_cmd.dc)
+        self.cmd.stdout.write(self.cmd.style.SUCCESS('Data cycle step %s created for data cycle %s (step_id=%s)' 
+            % (dc_step.step_id, self.dc_cmd.dc.id, dc_step.id)))
+        
+        """ Load list of all MLSs objects that will be downloaded/extracted """
+            
+        return dc_step
+
+
+        
+        
+                
